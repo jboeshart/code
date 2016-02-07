8e02589396fe6746972e0068b38feb4e94e04a0b
Jason Boeshart <jboeshart@users.noreply.github.com>
2016-02-07 16:35:11 -0600

    Update Microsoft.PowerShellISE_profile.ps1

--- powershell/Microsoft.PowerShellISE_profile.ps1
+++ powershell/Microsoft.PowerShellISE_profile.ps1
@@ -1,10 +1,9 @@
-
-
 # Start AzureAutomationISEAddOn snippet
 Import-Module AzureAutomationAuthoringToolkit
 # End AzureAutomationISEAddOn snippet
 
 # Set up history retention
+# Sourced from http://www.powershellmagazine.com/2014/07/14/pstip-automatically-persist-history/
 $PSHistoryPath = Join-Path (Split-Path -Path $profile -Parent) 'PS-History.csv'
 if (Test-Path -Path $PSHistoryPath) {
   Import-Csv -Path $PSHistoryPath | Add-History  
