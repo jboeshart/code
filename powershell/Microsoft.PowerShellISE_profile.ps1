b1aa55892f045441d2a09119357945829dc9ee6e
Jason Boeshart <jboeshart@users.noreply.github.com>
2016-02-07 16:33:37 -0600

    Create Microsoft.PowerShellISE_profile.ps1

---
+++ powershell/Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,14 @@
+
+
+# Start AzureAutomationISEAddOn snippet
+Import-Module AzureAutomationAuthoringToolkit
+# End AzureAutomationISEAddOn snippet
+
+# Set up history retention
+$PSHistoryPath = Join-Path (Split-Path -Path $profile -Parent) 'PS-History.csv'
+if (Test-Path -Path $PSHistoryPath) {
+  Import-Csv -Path $PSHistoryPath | Add-History  
+}
+Register-EngineEvent -SupportEvent PowerShell.Exiting -Action { 
+  Get-History -Count 500 | Export-Csv -Path $PSHistoryPath  
+}
