74b5032804ec8a30f3b0f6725cfa954a740c8ed7
Jason Boeshart <jboeshart@users.noreply.github.com>
2017-12-08 08:39:42 -0600

    Update blob-line-counter.ps1

--- azure/blob-line-counter.ps1
+++ azure/blob-line-counter.ps1
@@ -1,1 +1,23 @@
+<#
+blob-line-counter.ps1
+Jason Boeshart, Cloud Solution Architect, Microsoft
+Apache 2.0 License
 
+.SYNOPSIS
+    Counts the number of lines in blob files
+
+.DESCRIPTION
+    This script will count the number of lines in block blob files in an Azure storage container. 
+
+.PARAMETER StorageAccountName 
+    Name of the storage account containing the blobs
+
+.PARAMETER StorageAccountKey
+    Storage account key for the storage account
+
+.PARAMETER ContainerName
+    Name of the container in the storage account containing the blobs
+
+.LINK
+    https://github.com/jboeshart/code/blob/master/azure/blob-line-counter.ps1
+#>
